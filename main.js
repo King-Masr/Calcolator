df925146e8b1f4b868ff43f903cb4dde2d84108f
Aly Ahmed Aly <487242982@qaliobia4.moe.edu.eg>
2023-04-28 18:05:56 +0300

    Update main.js

--- main.js
+++ main.js
@@ -12,58 +12,75 @@ darkBtn.onclick = function () {
 let type = document.getElementById("type");
 let result = document.getElementById("result");
 let AC = document.getElementById("AC");
+AC.onclick = function () {
+  type.innerText = 0;
+  result.innerText = 0;
+};
 let Del = document.getElementById("Del");
 let Percentsign = document.getElementById("Percentsign");
 let Divisionsign = document.getElementById("Divisionsign");
 let Multiplicationsign = document.getElementById("Multiplicationsign");
+Multiplicationsign.onclick = function () {
+  if (type.innerText == 0) type.innerText = "";
+  type.innerText += "×";
+};
 let Minussign = document.getElementById("Minussign");
 let Plussign = document.getElementById("Plussign");
 let Equalsign = document.getElementById("Equalsign");
 let One = document.getElementById("One");
 One.onclick = function () {
+  if (type.innerText == 0) type.innerText = "";
   type.innerText += 1;
 }
 let Two = document.getElementById("Two");
 Two.onclick = function () {
+  if (type.innerText == 0) type.innerText = "";
   type.innerText += 2;
 };
 let Three = document.getElementById("Three");
 Three.onclick = function () {
+  if (type.innerText == 0) type.innerText = "";
   type.innerText += 3;
 };
 let Four = document.getElementById("Four");
 Four.onclick = function () {
+  if (type.innerText == 0) type.innerText = "";
   type.innerText += 4;
 };
 let Five = document.getElementById("Five");
 Five.onclick = function () {
+  if (type.innerText == 0) type.innerText = "";
   type.innerText += 5;
 };
 let Six = document.getElementById("Six");
 Six.onclick = function () {
+  if (type.innerText == 0) type.innerText = "";
   type.innerText += 6;
 };
 let Saven = document.getElementById("Saven");
 Saven.onclick = function () {
+  if (type.innerText == 0) type.innerText = "";
   type.innerText += 7;
 };
 let Eight = document.getElementById("Eight");
 Eight.onclick = function () {
+  if (type.innerText == 0) type.innerText = "";
   type.innerText += 8;
 };
 let Nine = document.getElementById("Nine");
 Nine.onclick = function () {
+  if (type.innerText == 0) type.innerText = "";
   type.innerText += 9;
 };
 let Zero = document.getElementById("Zero");
 Zero.onclick = function () {
-  type.innerText += 0;
+  if (type.innerText != 0) type.innerText += 0;
 };
 let DoubleZero = document.getElementById("DoubleZero");
 DoubleZero.onclick = function () {
-  type.innerText += 00;
+  if (type.innerText != 0) type.innerText += 00;
 };
 let Dot = document.getElementById("Dot");
 Dot.onclick = function () {
-  type.innerText += ".";
+  if (type.innerText != 0) type.innerText += ".";
 };
